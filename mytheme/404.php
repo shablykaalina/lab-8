9b5abbb6d83e1dff707de86deb9801d25b097f41
shablykaalina <sablykoa06@gmail.com>
2024-12-23 23:33:33 +0300

    ///

--- mytheme/404.php
+++ mytheme/404.php
@@ -3,9 +3,6 @@
 <head>
    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Магазин мягких вязаных игрушек из плюшевой пряжи ">
-    <meta name="keywords" content="Вязаные игрушки, мягкие игрушки, игрушки крючком, handmade игрушки,осьминог перевертыш, игрушка осьминог, игрушка пчела, игрушка авокадо, игрушка пингвин">
-    <meta name="author" content="Alina Shablyko">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/Фавикон.ico" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
